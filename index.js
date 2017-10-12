bf269b94454f4e98ed045169c948fcb12fb9d24f
Victor Guillem Agusti <vidasoft.app@gmail.com>
2017-10-12 17:22:38 +0200

    Versión de ejemplo que funciona.

--- index.js
+++ index.js
@@ -1,20 +1,11 @@
-// const Telegraf = require('telegraf')
+const Telegraf = require('telegraf');
 
-// const app = new Telegraf("470252108:AAEHri_wBBBU4XYrULE7DKgypUfS6zVmozs")
-// app.command('start', ({ from, reply }) => {
-//   console.log('start', from)
-//   return reply('Welcome!')
-// })
-// app.hears('hi', (ctx) => ctx.reply('Hey there!'))
-// app.on('sticker', (ctx) => ctx.reply('👍'))
-// app.startPolling()
-// const Telegraf = require('telegraf')
-// const { reply } = Telegraf
+const app = new Telegraf("470252108:AAEHri_wBBBU4XYrULE7DKgypUfS6zVmozs")
 
-// const bot = new Telegraf(process.env.BOT_TOKEN)
-// bot.command('/oldschool', (ctx) => ctx.reply('Hello'))
-// bot.command('/modern', ({ reply }) => reply('Yo'))
-// bot.command('/hipster', reply('λ'))
-// bot.startPolling()
-
-console.log("Hola mundo");
+app.command('start', ({ from, reply }) => {
+  console.log('start', from)
+  return reply('Bienvenido parguelilla!')
+})
+app.hears('holi', (ctx) => ctx.reply('Holi holi holiiii!'))
+app.on('sticker', (ctx) => ctx.reply('👍'))
+app.startPolling()
