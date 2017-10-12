5f88e6c8bbb4e81101f45ff8f0859964d99ebf8c
Victor Guillem Agusti <vidasoft.app@gmail.com>
2017-10-12 17:10:36 +0200

    Hola mundo para openshift

---
+++ index.js
@@ -0,0 +1,20 @@
+// const Telegraf = require('telegraf')
+
+// const app = new Telegraf("470252108:AAEHri_wBBBU4XYrULE7DKgypUfS6zVmozs")
+// app.command('start', ({ from, reply }) => {
+//   console.log('start', from)
+//   return reply('Welcome!')
+// })
+// app.hears('hi', (ctx) => ctx.reply('Hey there!'))
+// app.on('sticker', (ctx) => ctx.reply('👍'))
+// app.startPolling()
+// const Telegraf = require('telegraf')
+// const { reply } = Telegraf
+
+// const bot = new Telegraf(process.env.BOT_TOKEN)
+// bot.command('/oldschool', (ctx) => ctx.reply('Hello'))
+// bot.command('/modern', ({ reply }) => reply('Yo'))
+// bot.command('/hipster', reply('λ'))
+// bot.startPolling()
+
+console.log("Hola mundo");
